85cc3caaf0e61e188ab50a25efd68b17ee06b004
Peter Jungers <peter.jungers@pcc.edu>
2024-08-02 15:55:10 -0700

    Re-working models

--- nes_music/models.py
+++ nes_music/models.py
@@ -1,6 +1,83 @@
 from nes_music import db
 
 
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    developer_id = db.Column(db.Integer, db.ForeignKey("company.id"))
+    publisher_id = db.Column(db.Integer, db.ForeignKey("company.id"))
+    year = db.Column(db.Integer)
+
+    song = db.relationship("Song", back_populates="game")
+    developer = db.relationship("Company",
+                                foreign_keys=[developer_id],
+                                back_populates="as_dev")
+    publisher = db.relationship("Company",
+                                foreign_keys=[publisher_id],
+                                back_populates="as_pub")
+
+    def __repr__(self):
+        return (f"<id = {self.id},"
+                f"name = {self.name}")
+
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    game_id = db.Column(db.Integer, db.ForeignKey("game.id"))
+    composer_id = db.Column(db.Integer, db.ForeignKey("composer.id"))
+    arranger_id = db.Column(db.Integer, db.ForeignKey("arranger.id"))
+
+    game = db.relationship("Game", back_populates="song")
+    composer = db.relationship("Composer", back_populates="song")
+    arranger = db.relationship("Arranger", back_populates="song")
+
+    def __repr__(self):
+        return (f"<id = {self.id},"
+                f"name = {self.name},"
+                f"game_id = {self.game_id},"
+                f"game = {self.game.name}")
+
+
+class Composer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    song = db.relationship("Song", back_populates="composer")
+
+    def __repr__(self):
+        return (f"<id = {self.id},"
+                f"name = {self.name}")
+
+
+class Arranger(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    song = db.relationship("Song", back_populates="arranger")
+
+    def __repr__(self):
+        return (f"<id = {self.id},"
+                f"name = {self.name}")
+
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    as_dev = db.relationship("Game",
+                             foreign_keys=[Game.developer_id],
+                             back_populates="developer")
+    as_pub = db.relationship("Game",
+                             foreign_keys=[Game.publisher_id],
+                             back_populates="publisher")
+
+    def __repr__(self):
+        return (f"<id = {self.id},"
+                f"name = {self.name}")
+
+
+
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     game = db.Column(db.String)
