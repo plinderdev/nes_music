0b7f8051005a1c6d539009424fb7d20a1967c746
Peter Jungers <peter.jungers@pcc.edu>
2024-08-04 13:22:54 -0700

    Finished and understand query

--- nes_music/models.py
+++ nes_music/models.py
@@ -17,8 +17,8 @@ class Game(db.Model):
                                 back_populates="as_pub")
 
     def __repr__(self):
-        return (f"<id = {self.id},"
-                f"name = {self.name}")
+        return (f"<id = {self.id}, "
+                f"name = {self.name}>")
 
 
 class Song(db.Model):
@@ -30,10 +30,10 @@ class Song(db.Model):
     song_musician = db.relationship("SongMusician", back_populates="song")
 
     def __repr__(self):
-        return (f"<id = {self.id},"
-                f"name = {self.name},"
-                f"game_id = {self.game_id},"
-                f"game = {self.game.name}")
+        return (f"<id = {self.id}, "
+                f"name = {self.name}, "
+                f"game_id = {self.game_id}, "
+                f"name = {self.game.name}>")
 
 
 class SongMusician(db.Model):
@@ -50,6 +50,12 @@ class SongMusician(db.Model):
                                foreign_keys=[arranger_id],
                                back_populates="as_arranger")
 
+    def __repr__(self):
+        return (f"<id = {self.id}, "
+                f"song = {self.song.name}, "
+                f"composer = {self.composer}, "
+                f"arranger = {self.arranger}>")
+
 
 class Musician(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -63,8 +69,8 @@ class Musician(db.Model):
                                   back_populates="arranger")
 
     def __repr__(self):
-        return (f"<id = {self.id},"
-                f"name = {self.name}")
+        return (f"<id = {self.id}, "
+                f"name = {self.name}>")
 
 
 class Company(db.Model):
@@ -79,8 +85,8 @@ class Company(db.Model):
                              back_populates="publisher")
 
     def __repr__(self):
-        return (f"<id = {self.id},"
-                f"name = {self.name}")
+        return (f"<id = {self.id}, "
+                f"name = {self.name}>")
 
     # Info re relationship to same table using multiple FKs:
     # https://stackoverflow.com/questions/65941555/sqlalchemy-error-multiple-foreign-keys-references-to-the-same-table-and-column/65994756#65994756
@@ -99,9 +105,9 @@ class Video(db.Model):
     playlist_video = db.relationship("PlaylistVideo", back_populates="video")
 
     def __repr__(self):
-        return (f"<id = {self.id},"
-                f"game = {self.game},"
-                f"song = {self.song},"
+        return (f"<id = {self.id}, "
+                f"game = {self.game}, "
+                f"song = {self.song}, "
                 f"link = {self.link}>")
 
 
@@ -114,8 +120,8 @@ class Playlist(db.Model):
                                      back_populates="playlist")
 
     def __repr__(self):
-        return (f"<id = {self.id},"
-                f"name = {self.game},"
+        return (f"<id = {self.id}, "
+                f"name = {self.game}, "
                 f"link = {self.link}>")
 
 
@@ -126,3 +132,8 @@ class PlaylistVideo(db.Model):
 
     playlist = db.relationship("Playlist", back_populates="playlist_video")
     video = db.relationship("Video", back_populates="playlist_video")
+
+    def __repr__(self):
+        return (f"<id = {self.id}, "
+                f"playlist = {self.playlist.name}, "
+                f"video = {self.video.song_id}>")
--- nes_music/routes.py
+++ nes_music/routes.py
@@ -11,24 +11,19 @@ from nes_music.models import (Game, Song, SongMusician, Musician, Company,
 @app.route("/")
 @app.route("/index")
 def index():
-    Company_alias = aliased(Company, name="Company_alias")
-
     table = db.session.execute(
-        select(Game, Company_alias, Song, Company, Video) # SongMusician, Musician,
-        .outerjoin(Company, Game.developer_id == Company.as_dev)
-        .outerjoin(Company_alias, Game.publisher_id == Company_alias.as_pub)
-        .filter(
-            Song.game_id == Game.id,
-            # SongMusician.song_id == Song.id,
-            # Musician.id == SongMusician.composer_id,
-            # Game.developer_id == Company.as_dev,
-            # Game.publisher_id == Company_alias.as_pub,
-            Video.song_id == Song.id
-        )
-        .order_by(Video.upload_date.desc())
-    ).all()
+            select(Game, Song, SongMusician, Video)
+            .outerjoin(Musician, SongMusician.composer_id == Musician.as_composer)
+            .filter(
+                Song.game_id == Game.id,
+                SongMusician.song_id == Song.id,
+                Video.song_id == Song.id
+                )
+            .order_by(Video.upload_date.desc())
+            ).all()
 
     for t in table:
-        print(t.Game.name, t. Song.name, t.Game.developer.name, t.Game.publisher.name)
+        if t.SongMusician.composer:
+            print(t.SongMusician.composer.name)
 
     return render_template("index.html", table=table)
