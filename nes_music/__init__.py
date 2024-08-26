38dda107e54f71f8141e716a44c0d061155a06d6
Plinder <plindermail@gmail.com>
2024-08-26 12:08:42 -0700

    Added error pages

--- nes_music/__init__.py
+++ nes_music/__init__.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///nes_music.db"
 db = SQLAlchemy(app)
 
-# from nes_music import errors
+from nes_music import errors
 from nes_music import models
 from nes_music import routes
